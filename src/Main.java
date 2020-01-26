dbcdb94353f3c9290f6b6aa0ab63bc20fea8daaa
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-01-26 22:37:49 +0300

    add shit code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final Scanner scanner = new Scanner(System.in);
+        ArrayList<Long> arrayList = new ArrayList<>();
+        while (scanner.hasNextLong()) arrayList.add(scanner.nextLong());
+        System.out.printf("Total numbers: %d.", arrayList.size());
+        long max = Collections.max(arrayList);
+        int count = 0;
+        for (Long number : arrayList) if (number == max) count++;
+        System.out.printf("The greatest number: %d (%d time(s)).", max, count);
     }
 }
